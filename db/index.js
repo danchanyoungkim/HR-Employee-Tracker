52535c24cc6479a38a6bafbe0c3ec9df53ecbbf6
Dan Kim <danchanyoungkim@gmail.com>
2022-03-23 21:03:21 -0400

    deleted todo

--- db/index.js
+++ db/index.js
@@ -9,10 +9,6 @@ class DB {
   // Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
   findAllEmployees() {
     return this.connection.query(
-      // CREATE SELECT STATMENT WITH THE FOLLOWING COLUMNS FROM THREE TABLES.
-      // id, first_name, last_name FROM employee TABLE AND department name from department TABLE AND SELECT salary FROM role TABLE
-      // YOUR NEED TO USE LEFT JOINS TO JOIN THREE TABLES
-      // TODO: YOUR CODE HERE
       "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary \
       FROM employee LEFT JOIN role ON employee.role_id = role.id \
       LEFT JOIN department ON role.department_id = department.id"
@@ -52,10 +48,6 @@ class DB {
   // Find all roles, join with departments to display the department name
   findAllRoles() {
     return this.connection.query(
-      // SELECT THE FOLLOWING COLUMNS:
-      // id, title, salary FROM role TABLE AND department name FROM department TABLE
-      // YOU NEED TO USE LEFT JOIN TO JOIN role and department TABLES
-      // TODO: YOUR CODE HERE
       "SELECT role.id, role.title, role.salary, department.name \
       FROM role LEFT JOIN department ON role.department_id = department.id"
     );
@@ -101,7 +93,6 @@ class DB {
   // Find all employees by manager, join with departments and roles to display titles and department names
   findAllEmployeesByManager(managerId) {
     return this.connection.query(
-      // TODO: YOUR CODE HERE
       "SELECT employee.id, employee.first_name, employee.last_name FROM employee \
       LEFT JOIN role on employee.role_id = role.id \
       LEFT JOIN department department on role.department_id = department.id \
--- index.js
+++ index.js
@@ -122,9 +122,6 @@ async function viewEmployeesByDepartment() {
   const departments = await db.findAllDepartments();
 
   const departmentChoices = departments.map(({ id, name }) => ({
-    // CREATE TWO PROPERTIES name AND value FOR THIS OBJECT. THE PROPERTY name SHOULD CONTAIN THE NAME OF THE DEPARTMENT.
-    // THE PROPERTY value SHOULD CONTAIN id.
-    // TODO: YOUR CODE HERE
     name: name,
     value: id
   }));
@@ -150,10 +147,6 @@ async function updateEmployeeRole() {
   const employees = await db.findAllEmployees();
 
   const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
-    // CREATE TWO PROPERTIES name AMD value FOR THIS OBJECT. THE PROPERTY name SHOULD CONTAIN THE CONCATENATION OF THE FIRST HAME AND THE LAST NAME.
-    // THE PROPERTY value SHOULD CONTAIN id.
-    // THIS OBJECT FOR EACH MANAGER WILL RETURN TO MAP() TO CONSTRUCT AN ARRAY TO BE RETURNED AND BE STORED TO managerChoices.
-    // TODO: YOUR CODE HERE
     name: first_name + ' ' + last_name,
     value: id
   }));
@@ -285,10 +278,6 @@ async function addEmployee() {
   employee.role_id = roleId;
 
   const managerChoices = employees.map(({ id, first_name, last_name }) => ({
-    // CREATE TWO PROPERTIES name AMD value FOR THIS OBJECT. THE PROPERTY name SHOULD CONTAIN THE CONCATENATION OF THE FIRST HAME AND THE LAST NAME.
-    // THE PROPERTY value SHOULD CONTAIN id.
-    // THIS OBJECT FOR EACH MANAGER WILL RETURN TO MAP() TO CONSTRUCT AN ARRAY TO BE RETURNED AND BE STORED TO managerChoices.
-    // TODO: YOUR CODE HERE
     name: first_name + ' ' + last_name,
     value: id
   }));
